c274b904ca884b174cc972aff204797f9fc5aa93
elamahpla <theamazingness@gmail.com>
2019-01-29 12:24:41 -0600

    Coding assessment. Last thing to work on is changing the count when a task is completed

--- src/App.js
+++ src/App.js
@@ -1,28 +1,94 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-class App extends Component {
+export default class TodoList extends Component {
+  state = {
+    term: "",
+    items: [],
+    completed: false,
+    active: false
+  };
+  onChange = event => {
+    this.setState({
+      term: event.target.value
+    });
+  };
+  onSubmit = event => {
+    event.preventDefault();
+    if (this.state.term === "") {
+      alert("Cannot accept an empty field, please add a task.");
+      return null;
+    }
+    let itemToAdd = {
+      isDone: false,
+      term: this.state.term
+    };
+    this.setState({
+      term: "",
+      items: [...this.state.items, itemToAdd]
+    });
+  };
+  toggleClass = item => {
+    this.setState(state => {
+      state.items[item].isDone = !state.items[item].isDone;
+      return state;
+    });
+  };
+  isCompleted = () => {
+    if (this.state.completed === true) {
+      return this.state.item.length - 1;
+    }
+    this.setState({
+      completed: this.state.completed
+    });
+  };
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <div>
+          <form onSubmit={this.onSubmit}>
+            <input
+              value={this.state.term}
+              onChange={this.onChange}
+              placeholder="Add a task"
+            />
+            <button type="submit">Add</button>
+          </form>
+          {this.state.items.length} remaining out of {this.state.items.length}{" "}
+          tasks
+        </div>
+        <ul>
+          {this.state.items.map((item, index) => (
+            <li
+              className={item.isDone ? "is-done" : ""}
+              onClick={() => this.toggleClass(index)}
+              key={index}
+            >
+              {item.term}
+            </li>
+          ))}
+        </ul>
+        <style>
+          {`
+            .is-done {
+                text-decoration: line-through;
+                  }
+                  ul {
+                      list-style: none;
+                  }
+                  li {
+                      font-size: 2rem;
+                  }
+                  button {
+                      background: green;
+                      color: white;
+                      border: none;
+                      font-size: 1.1rem;
+                      margin: 5px 0 0 2px;
+                  }
+              `}
+        </style>
       </div>
     );
   }
 }
-
-export default App;
